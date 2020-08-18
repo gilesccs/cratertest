a91203eae850df3603aaea7a92fd509ee784e058
Giles Chang <gileschang97@hotmail.com>
2020-08-18 21:00:12 +0800

    Added Basic configuration and header component

---
+++ frontend/src/components/header/header.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./../../css/bootstrap.min.css";
+import "./../../css/font-awesome.min.css";
+import './../../css/themify-icons.css';
+import './../../css/elegant-icons.css';
+// import './../../css/owl.carousel.min.css';
+import "./../../css/nice-select.css";
+// import './../../css/jquery-ui.min.css';
+import "./../../css/slicknav.min.css";
+import "./../../css/style.css";
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <div className="header-top">
+          <div className="container">
+            <div className="ht-left">
+              <div className="mail-service">
+                <i className=" fa fa-envelope"></i>
+                hello.colorlib@gmail.com
+              </div>
+              <div className="phone-service">
+                <i className=" fa fa-phone"></i>
+                +65 11.188.888
+              </div>
+            </div>
+            <div className="ht-right">
+              <a href="#" className="login-panel">
+                <i className="fa fa-user"></i>Login
+              </a>
+              <div className="top-social">
+                <a href="#">
+                  <i className="ti-facebook"></i>
+                </a>
+                <a href="#">
+                  <i className="ti-twitter-alt"></i>
+                </a>
+                <a href="#">
+                  <i className="ti-linkedin"></i>
+                </a>
+                <a href="#">
+                  <i className="ti-pinterest"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
---
+++ frontend/src/pages/homepage/homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Homepage() {
+    return (
+      <div >
+        <header >
+          hi
+        </header>
+      </div>
+    );
+  }
+  
+  export default Homepage;
