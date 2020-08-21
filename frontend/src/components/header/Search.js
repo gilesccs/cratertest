81f865078500a3f54716855f83c3c581aabcf361
Alwyn-Ong <52643926+Alwyn-Ong@users.noreply.github.com>
2020-08-21 23:48:45 +0800

    Added navbar

--- frontend/src/components/header/Search.js
+++ frontend/src/components/header/Search.js
@@ -1,98 +1,117 @@
 import React from "react";
 import "./../../css/bootstrap.min.css";
 import "./../../css/font-awesome.min.css";
-import './../../css/themify-icons.css';
-import './../../css/elegant-icons.css';
+import "./../../css/themify-icons.css";
+import "./../../css/elegant-icons.css";
 // import './../../css/owl.carousel.min.css';
 import "./../../css/nice-select.css";
 // import './../../css/jquery-ui.min.css';
 import "./../../css/slicknav.min.css";
 import "./../../css/style.css";
 
-function Search() {
-    return (
-        <div class="container">
-            <div class="inner-header">
-                <div class="row">
-                    <div class="col-lg-2 col-md-2">
-                        <div class="logo">
-                            <a href="./index.html">
-                                <img src={require('../../img/logo.png')} alt=""/> 
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-lg-7 col-md-7">
-                        <div class="advanced-search">
-                            <button type="button" class="category-btn">All Categories</button>
-                            <div class="input-group">
-                                <input type="text" placeholder="What do you need?"/>
-                                <button type="button"><i class="ti-search"></i></button>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 text-right col-md-3">
-                        <ul class="nav-right">
-                            <li class="heart-icon">
-                                <a href="#">
-                                    <i class="icon_heart_alt"></i>
-                                    <span>1</span>
-                                </a>
-                            </li>
-                            <li class="cart-icon">
-                                <a href="#">
-                                    <i class="icon_bag_alt"></i>
-                                    <span>3</span>
-                                </a>
-                                <div class="cart-hover">
-                                    <div class="select-items">
-                                        <table>
-                                            <tbody>
-                                                <tr>
-                                                    <td class="si-pic"><img src={require("../../img/select-product-1.jpg")} alt=""/></td>
-                                                    <td class="si-text">
-                                                        <div class="product-selected">
-                                                            <p>$60.00 x 1</p>
-                                                            <h6>Kabino Bedside Table</h6>
-                                                        </div>
-                                                    </td>
-                                                    <td class="si-close">
-                                                        <i class="ti-close"></i>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="si-pic"><img src={require("../../img/select-product-2.jpg")} alt=""/></td>
-                                                    <td class="si-text">
-                                                        <div class="product-selected">
-                                                            <p>$60.00 x 1</p>
-                                                            <h6>Kabino Bedside Table</h6>
-                                                        </div>
-                                                    </td>
-                                                    <td class="si-close">
-                                                        <i class="ti-close"></i>
-                                                    </td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                    <div class="select-total">
-                                        <span>total:</span>
-                                        <h5>$120.00</h5>
-                                    </div>
-                                    <div class="select-button">
-                                        <a href="#" class="primary-btn view-card">VIEW CARD</a>
-                                        <a href="#" class="primary-btn checkout-btn">CHECK OUT</a>
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="cart-price">$150.00</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
+// To change:
+// Links, img links
 
-    );
+function Search() {
+  return (
+    <div class="container">
+      <div class="inner-header">
+        <div class="row">
+          <div class="col-lg-2 col-md-2">
+            <div class="logo">
+              <a href="./index.html">
+                <img src={require("../../img/logo.png")} alt="" />
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-7 col-md-7">
+            <div class="advanced-search">
+              <button type="button" class="category-btn">
+                All Categories
+              </button>
+              <div class="input-group">
+                <input type="text" placeholder="What do you need?" />
+                <button type="button">
+                  <i class="ti-search"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 text-right col-md-3">
+            <ul class="nav-right">
+              <li class="heart-icon">
+                <a href="#">
+                  <i class="icon_heart_alt"></i>
+                  <span>1</span>
+                </a>
+              </li>
+              <li class="cart-icon">
+                <a href="#">
+                  <i class="icon_bag_alt"></i>
+                  <span>3</span>
+                </a>
+                <div class="cart-hover">
+                  <div class="select-items">
+                    <table>
+                      <tbody>
+                        <tr>
+                          <td class="si-pic">
+                            <img
+                              src={require("../../img/select-product-1.jpg")}
+                              alt=""
+                            />
+                          </td>
+                          <td class="si-text">
+                            <div class="product-selected">
+                              <p>$60.00 x 1</p>
+                              <h6>Kabino Bedside Table</h6>
+                            </div>
+                          </td>
+                          <td class="si-close">
+                            <i class="ti-close"></i>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="si-pic">
+                            <img
+                              src={require("../../img/select-product-2.jpg")}
+                              alt=""
+                            />
+                          </td>
+                          <td class="si-text">
+                            <div class="product-selected">
+                              <p>$60.00 x 1</p>
+                              <h6>Kabino Bedside Table</h6>
+                            </div>
+                          </td>
+                          <td class="si-close">
+                            <i class="ti-close"></i>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                  <div class="select-total">
+                    <span>total:</span>
+                    <h5>$120.00</h5>
+                  </div>
+                  <div class="select-button">
+                    <a href="#" class="primary-btn view-card">
+                      VIEW CARD
+                    </a>
+                    <a href="#" class="primary-btn checkout-btn">
+                      CHECK OUT
+                    </a>
+                  </div>
+                </div>
+              </li>
+              <li class="cart-price">$150.00</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Search;
-
--- frontend/src/pages/homepage/homepage.js
+++ frontend/src/pages/homepage/homepage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './../../components/header/header';
 import Search from './../../components/header/Search';
+import NavBar from './../../components/header/navbar';
 
 function Homepage() {
     return (
       <div >
         <Header></Header>
         <Search></Search>
+        <NavBar></NavBar>
       </div>
     );
   }
