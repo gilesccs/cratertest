c78c0f33874177bb87517a2b9bbb4df1b133e797
Alwyn-Ong <52643926+Alwyn-Ong@users.noreply.github.com>
2020-08-23 19:17:20 +0800

    Fixed dependencies for external plugins

--- frontend/src/components/homepage/benefits.js
+++ frontend/src/components/homepage/benefits.js
@@ -3,9 +3,9 @@ import "./../../css/bootstrap.min.css";
 import "./../../css/font-awesome.min.css";
 import './../../css/themify-icons.css';
 import './../../css/elegant-icons.css';
-import './../../css/owl.carousel.min.css';
+// import './../../css/owl.carousel.min.css';
 import "./../../css/nice-select.css";
-import './../../css/jquery-ui.min.css';
+// import './../../css/jquery-ui.min.css';
 import "./../../css/slicknav.min.css";
 import "./../../css/style.css";
 
